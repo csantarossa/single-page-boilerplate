064a8d55fa11ac8ac9bf8ac8109d7c4ec80a626f
COREY SANTAROSSA <santarossacorey@gmail.com>
2024-05-08 15:57:26 +1000

    updated email

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import LandingPage from "./pages/LandingPage";
 export const ThemeContext = createContext();
 
 function App() {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   return (
     <ThemeContext.Provider value={[theme, setTheme]}>
