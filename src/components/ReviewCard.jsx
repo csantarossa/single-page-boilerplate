ffaf00b40e175d805d6a9d6ce54340c2056e8db7
COREY SANTAROSSA <santarossacorey@gmail.com>
2024-05-09 15:36:27 +1000

    review fix

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,19 +3,35 @@ import React from "react";
 const ReviewCard = ({ name, description, rating, img }) => {
   return (
     <div className="card h-[50vh] card-compact w-80 lg:w-96 bg-base-100 shadow-xl hover:scale-105 duration-200">
-      <figure>
-        <img src={img} className="h-fit " alt="images" />
+      <figure className="w-full h-56 overflow-hidden">
+        <img src={img} className="h-auto w-full object-cover " alt="images" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>"{description}"</p>
         <div className="card-actions justify-end">
           <div className="rating">
-            <input className="mask mask-star-2 bg-orange-400" />
-            <input className="mask mask-star-2 bg-orange-400" />
-            <input className="mask mask-star-2 bg-orange-400" />
-            <input className="mask mask-star-2 bg-orange-400" />
-            <input className="mask mask-star-2 bg-orange-400" checked />
+            <input
+              disabled
+              className="mask mask-star-2 bg-orange-400 cursor-default"
+            />
+            <input
+              disabled
+              className="mask mask-star-2 bg-orange-400 cursor-default"
+            />
+            <input
+              disabled
+              className="mask mask-star-2 bg-orange-400 cursor-default"
+            />
+            <input
+              disabled
+              className="mask mask-star-2 bg-orange-400 cursor-default"
+            />
+            <input
+              disabled
+              className="mask mask-star-2 bg-orange-400 cursor-default"
+              checked
+            />
           </div>
         </div>
       </div>
